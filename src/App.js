bd39d5ef23fa2541e95fd5a39a181306afa8d758
Jack Tiebie <jacktiebie45@gmail.com>
2022-04-19 15:52:57 +0200

    Yes to No code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import ReactDOM from 'react-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [isImportant, setIsImportant] = React.useState("Yes")
+    /**
+     * Challenge: 
+     * 1. Create a function called `handleClick` that runs
+     *    setIsImportant("No")
+     * 2. add a click event listener to the div.state--value
+     *    that runs `handleClick` when the div is clicked.
+     */
+    
+    function handleClick() {
+        setIsImportant('No')
+    }
+    
+    return (
+        <div className="state">
+            <h1 className="state--title">Is state important to know?</h1>
+            <div onClick={handleClick} className="state--value">
+                <h1>{isImportant}</h1>
+            </div>
+        </div>
+    )
 }
-
-export default App;
